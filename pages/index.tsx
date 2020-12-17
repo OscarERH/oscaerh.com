4c27c111b463dce57c50ae4f60bc8d8213210af1
OscarERH <oscareruizh@gmail.com>
2020-12-17 14:08:55 -0400

    Change text color

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,9 @@ import { ReactElement } from 'react'
 export default function Home(): ReactElement {
   return (
     <div>
-      <h1 className="text-indigo-300">Ola fea</h1>
+      <h1 className="text-gray-900 font-thin">
+        OscarERH
+      </h1>
     </div>
   )
 }
